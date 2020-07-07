04d6ec087f164eba377ab77db2d28d17e8e3c085
Isaiah Suarez <isaiahsuarez@fb.com>
2020-07-07 12:22:11 -0700

    Posts are queried from Parse.

--- app/src/main/java/com/example/parstagram/MainActivity.java
+++ app/src/main/java/com/example/parstagram/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.parstagram;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.parstagram.databinding.ActivityMainBinding;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -19,6 +25,26 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
         // Done with view binding
+
+        queryPosts();
     }
-    
+
+    private void queryPosts() {
+        ParseQuery<Post> postParseQuery = ParseQuery.getQuery(Post.class);
+        postParseQuery.include(Post.KEY_USER);
+
+        postParseQuery.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+
+                for (Post post : posts) {
+                    Log.i(TAG, "Post: " + post.getDescription() + " username: " + post.getUser().getUsername());
+                }
+            }
+        });
+    }
 }
