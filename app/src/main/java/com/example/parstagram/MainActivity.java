091afca4c18cdae5f6504f0699d2339f7982e87f
Isaiah Suarez <isaiahsuarez@fb.com>
2020-07-08 14:43:02 -0700

    Extracted Posting UI into ComposeFragment.

--- app/src/main/java/com/example/parstagram/MainActivity.java
+++ app/src/main/java/com/example/parstagram/MainActivity.java
@@ -10,15 +10,19 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
 
 import com.example.parstagram.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
@@ -33,12 +37,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
   private static final String TAG = "MainActivity";
 
-  public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 4;
-  public String photoFileName = "photo.jpg";
-
   ActivityMainBinding binding;
-
-  private File photoFile;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -48,116 +47,30 @@ public class MainActivity extends AppCompatActivity {
     setContentView(view);
     // Done with view binding
 
-//        queryPosts();
+    setupNavigationView();
   }
 
-  // User pressed the "Post" button
-  public void onPostClick(View view) {
-    String description = binding.etDescription.getText().toString();
-    if (description.isEmpty()) {
-      Toast.makeText(this, "Description cannot be empty", Toast.LENGTH_SHORT).show();
-      return;
-    }
-    if (photoFile == null || binding.ivPostImage.getDrawable() == null) {
-      Toast.makeText(this, "There is no image", Toast.LENGTH_SHORT).show();
-      return;
-    }
-    ParseUser currentUser = ParseUser.getCurrentUser();
-    savePost(description, photoFile, currentUser);
+  private void setupNavigationView() {
 
-  }
-
-  private void savePost(String description, File photoFile, ParseUser currentUser) {
-    Post post = new Post();
-    post.setDescription(description);
-    post.setImage(new ParseFile(photoFile));
-    post.setUser(currentUser);
-    post.saveInBackground(new SaveCallback() {
+    binding.bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
       @Override
-      public void done(ParseException e) {
-        if (e != null) {
-          Log.e(TAG, "Error while saving", e);
-          Toast.makeText(MainActivity.this, "Error while saving", Toast.LENGTH_SHORT).show();
-          return;
+      public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment;
+        switch (menuItem.getItemId()) {
+          case R.id.action_home:
+            break;
+          case R.id.action_compose:
+            break;
+          case R.id.action_profile:
+            break;
+          default:
+            break;
         }
-        Log.i(TAG, "Post saved successful!");
-        binding.etDescription.setText("");
-        binding.ivPostImage.setImageResource(0);
+        return true;
       }
     });
   }
 
-  // User pressed the "Take Picture" button
-  public void onTakePictureClick(View view) {
-    launchCamera();
-  }
-
-  private void launchCamera() {
-    // create Intent to take a picture and return control to the calling application
-    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-    // Create a File reference for future access
-    photoFile = getPhotoFileUri(photoFileName);
-
-    // wrap File object into a content provider
-    // required for API >= 24
-    // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
-    Uri fileProvider = FileProvider.getUriForFile(MainActivity.this, "com.codepath.fileprovider", photoFile);
-    intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
-
-    // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-    // So as long as the result is not null, it's safe to use the intent.
-    if (intent.resolveActivity(getPackageManager()) != null) {
-      // Start the image capture intent to take photo
-      startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-
-    }
-  }
-
-  @Override
-  protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-    super.onActivityResult(requestCode, resultCode, data);
-    if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-      if (resultCode == RESULT_OK) {
-        // by this point we have the camera photo on disk
-        Bitmap takenImage = rotateBitmapOrientation(photoFile.getAbsolutePath());
-
-        // RESIZE BITMAP, see section below
-        // Load the taken image into a preview
-        binding.ivPostImage.setImageBitmap(takenImage);
-      } else { // Result was a failure
-        Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
-      }
-    }
-  }
-
-  public Bitmap rotateBitmapOrientation(String photoFilePath) {
-    // Create and configure BitmapFactory
-    BitmapFactory.Options bounds = new BitmapFactory.Options();
-    bounds.inJustDecodeBounds = true;
-    BitmapFactory.decodeFile(photoFilePath, bounds);
-    BitmapFactory.Options opts = new BitmapFactory.Options();
-    Bitmap bm = BitmapFactory.decodeFile(photoFilePath, opts);
-    // Read EXIF Data
-    ExifInterface exif = null;
-    try {
-      exif = new ExifInterface(photoFilePath);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    String orientString = exif.getAttribute(ExifInterface.TAG_ORIENTATION);
-    int orientation = orientString != null ? Integer.parseInt(orientString) : ExifInterface.ORIENTATION_NORMAL;
-    int rotationAngle = 0;
-    if (orientation == ExifInterface.ORIENTATION_ROTATE_90) rotationAngle = 90;
-    if (orientation == ExifInterface.ORIENTATION_ROTATE_180) rotationAngle = 180;
-    if (orientation == ExifInterface.ORIENTATION_ROTATE_270) rotationAngle = 270;
-    // Rotate Bitmap
-    Matrix matrix = new Matrix();
-    matrix.setRotate(rotationAngle, (float) bm.getWidth() / 2, (float) bm.getHeight() / 2);
-    Bitmap rotatedBitmap = Bitmap.createBitmap(bm, 0, 0, bounds.outWidth, bounds.outHeight, matrix, true);
-    // Return result
-    return rotatedBitmap;
-  }
 
   private void queryPosts() {
     ParseQuery<Post> postParseQuery = ParseQuery.getQuery(Post.class);
@@ -178,23 +91,5 @@ public class MainActivity extends AppCompatActivity {
     });
   }
 
-  // Returns the File for a photo stored on disk given the fileName. URI = uniform resource identifier
-  private File getPhotoFileUri(String photoFileName) {
-
-    // Get safe storage directory for photos
-    // Use `getExternalFilesDir` on Context to access package-specific directories.
-    // This way, we don't need to request external read/write runtime permissions.
-    File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
-
-    // Create the storage directory if it does not exist
-    if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
-      Log.d(TAG, "failed to create directory");
-    }
-
-    // Return the file target for the photo based on filename
-    File file = new File(mediaStorageDir.getPath() + File.separator + photoFileName);
-
-    return file;
-  }
 
 }
